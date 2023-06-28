e838f6b82a575dfdc133255735143532d97144c8
Rajat Pandey <rajatpandey7800@gmail.com>
2023-06-28 22:24:24 +0530

    VaraibleNames added

---
+++ 1.Introduction/1.Introduction.py
@@ -0,0 +1,39 @@
+## Print Hello World
+print("Hello World")
+
+## Data Types ##
+
+# 1. Primitive
+# 2. Non Primitive
+'''
+Text Type:	str
+Numeric Types:	int, float, complex
+Sequence Types:	list, tuple, range
+Mapping Type:	dict
+Set Types:	set, frozenset
+Boolean Type:	bool
+Binary Types:	bytes, bytearray, memoryview
+None Type:	NoneType
+'''
+
+a = 10
+b = 20.33
+c = "Rajat"
+d = 1j
+e = ["apple", "banana", "cherry"]
+f = ("apple", "banana", "cherry")
+g = {"name" : "John", "age" : 36}
+h = {"apple", "banana", "cherry"}
+i = True
+
+## Print Value
+
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+print(type(e))
+print(type(f))
+print(type(g))
+print(type(h))
+print(type(i))
---
+++ 1.Introduction/2.VariableNames.py
@@ -0,0 +1,40 @@
+# A variable can have a short name (like x and y) or a more descriptive name (age, carname, total_volume). Rules for Python variables:
+# 1. A variable name must start with a letter or the underscore character
+# 2. A variable name cannot start with a number
+# 3. A variable name can only contain alpha-numeric characters and underscores (A-z, 0-9, and _ )
+# 4. Variable names are case-sensitive (age, Age and AGE are three different variables)
+# 5. A variable name cannot be any of the Python keywords.
+
+# all are valid variable names
+myvar = "Jai Shree Ram"
+my_var = "Jai Shree Ram"
+_my_var = "Jai Shree Ram"
+myVar = "Jai Shree Ram"
+MYVAR = "Jai Shree Ram"
+myvar2 = "Jai Shree Ram"
+
+print(myvar)
+print(my_var)
+print(_my_var)
+print(myVar)
+print(MYVAR)
+print(myvar2)
+
+# Illegal Variable names
+# 2myvar = "Jai Shree Ram"
+# my-var = "Jai Shree Ram"
+# my var = "Jai Shree Ram"
+
+
+# Camel Case
+# Each word, except the first, starts with a capital letter:
+
+myVariableName = "Jai Shree Ram"
+# Pascal Case
+# Each word starts with a capital letter:
+
+MyVariableName = "Jai Shree Ram"
+# Snake Case
+# Each word is separated by an underscore character:
+
+my_variable_name = "Jai Shree Ram"

272b4efcdc654d4ba6df54199d7291b84c3ffb0c
Rajat Pandey <rajatpandey7800@gmail.com>
2023-09-02 17:49:32 +0530

    2D list

---
+++ 6.MultiDimensionalList/1.Introduction.py
@@ -0,0 +1,4 @@
+# A list can have another list within
+
+li = [[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12], [13, 14, 15, 16]]
+print(li[3])
---
+++ 6.MultiDimensionalList/4.InputInTwoDimensionalList.py
@@ -0,0 +1,5 @@
+row = int(input())
+col = int(input())
+
+list_2d = [[int(x) for x in input().split()] for i in range(row) ]
+print(list_2d) #output - [[1, 2], [3, 4]]

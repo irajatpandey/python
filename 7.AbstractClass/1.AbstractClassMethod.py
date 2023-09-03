9c73509dccbaa112e76877a4805832afe0ee562f
Rajat Pandey <rajatpandey7800@gmail.com>
2023-09-03 18:17:28 +0530

    OOPS

--- 7.AbstractClass/1.AbstractClassMethod.py
+++ 7.AbstractClass/1.AbstractClassMethod.py
@@ -11,11 +11,20 @@ class SBI(Bank):
     def calculateRateOfInterest(self):
         return self.__roi * 12
 
-s1 = SBI(7)
+class BOI(Bank):
+    def __init__(self, roi):
+        self.__roi = roi
+    
+    def calculateRateOfInterest(self):
+        return self.__roi * 5
+    
 
 class HDFC(Bank):
     pass
-
+s1 = SBI(7)
 print(s1.calculateRateOfInterest())
 
-h1 = HDFC() #Can't instantiate abstract class HDFC with abstract method calculateRateOfInterest
+s2 = BOI(8)
+print(s2.calculateRateOfInterest())
+
+#h1 = HDFC() #Can't instantiate abstract class HDFC with abstract method calculateRateOfInterest

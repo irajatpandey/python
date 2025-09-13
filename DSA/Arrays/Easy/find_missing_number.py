ec6fd477734a069f097a50ccf92f23057ccc6cdf
Rajat Pandey <irajatnitjsr@gmail.com>
2025-09-13 13:51:01 +0530

    find missing number

--- DSA/Arrays/Easy/find_missing_number.py
+++ DSA/Arrays/Easy/find_missing_number.py
@@ -1,5 +1,12 @@
 def find_missing_number(nums: list) -> int:
-    pass
+    
+    sum = 0
+    for item in nums:
+        sum += item
+        
+    n = len(nums)
+    total = (n * (n + 1)) // 2
+    return total - sum
 
 if __name__ == "__main__":
     # Example data

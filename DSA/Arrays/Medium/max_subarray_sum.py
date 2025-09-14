7d73cf5e594e7fbbc499da47bcbdeb772eaacef3
Rajat Pandey <irajatnitjsr@gmail.com>
2025-09-14 11:16:05 +0530

    Kadane's Algo

--- DSA/Arrays/Medium/max_subarray_sum.py
+++ DSA/Arrays/Medium/max_subarray_sum.py
@@ -4,7 +4,15 @@ def max_subarray_sum(nums: List[int]) -> int:
     """
     Finds the contiguous subarray with the largest sum using Kadane's Algorithm.
     """
-    pass
+
+    max_sum_so_far = float("-inf")
+    current_sum = 0
+
+    for item in nums:
+        current_sum = max(current_sum + item, item)
+        max_sum_so_far = max(max_sum_so_far, current_sum)
+
+    return max_sum_so_far
 
 if __name__ == "__main__":
     nums = [-2, 1, -3, 4, -1, 2, 1, -5, 4]

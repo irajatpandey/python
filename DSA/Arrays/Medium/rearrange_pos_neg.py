6a843f3313b30926fa1b5e80ae48c1f6ebba441a
Rajat Pandey <irajatnitjsr@gmail.com>
2025-09-14 21:02:26 +0530

    https://leetcode.com/problems/rearrange-array-elements-by-sign/description/

--- DSA/Arrays/Medium/rearrange_pos_neg.py
+++ DSA/Arrays/Medium/rearrange_pos_neg.py
@@ -1,17 +1,27 @@
 from typing import List
 
-def max_profit(prices: List[int]) -> int:
-    """
-    Calculates the maximum profit from buying and selling a stock.
-    You can only complete at most one transaction.
-    """
-    pass
+def rearrangeArray(nums: List[int]) -> List[int]:
+    n = len(nums)
+    result = [0] * n  # Result array with same size as nums
 
-if __name__ == "__main__":
-    prices = [7, 1, 5, 3, 6, 4]
-    profit = max_profit(prices)
-    print(f"The maximum profit is: {profit}")
+    pos_ptr = 0
+    neg_ptr = 1
 
-    prices = [7, 6, 4, 3, 1]
-    profit = max_profit(prices)
-    print(f"The maximum profit is: {profit}")
+    for num in nums:
+        if num > 0:
+            result[pos_ptr] = num
+            pos_ptr += 2
+        else: # num < 0
+            result[neg_ptr] = num
+            neg_ptr += 2
+    
+    return result
+
+# Example usage
+nums1 = [3, 1, -2, -5, 2, -4]
+print(rearrangeArray(nums1))
+# Output: [3, -2, 1, -5, 2, -4]
+
+nums2 = [-1, 1]
+print(rearrangeArray(nums2))
+# Output: [1, -1]

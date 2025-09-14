be04493642b716041f6ef20967cd6c6b178b0f3d
Rajat Pandey <irajatnitjsr@gmail.com>
2025-09-14 17:39:55 +0530

    https://leetcode.com/problems/best-time-to-buy-and-sell-stock/

--- DSA/Arrays/Medium/stock_buy_sell.py
+++ DSA/Arrays/Medium/stock_buy_sell.py
@@ -5,7 +5,12 @@ def max_profit(prices: List[int]) -> int:
     Calculates the maximum profit from buying and selling a stock.
     You can only complete at most one transaction.
     """
-    pass
+    current_min_price = float('inf')
+    max_profit = 0
+    for price in prices:
+        current_min_price = min(current_min_price, price)
+        max_profit = max(max_profit, price - current_min_price)
+    return max_profit
 
 if __name__ == "__main__":
     prices = [7, 1, 5, 3, 6, 4]

e19cd68cca01f6b5759bd7c8195bea31f287c603
Rajat Pandey <irajatnitjsr@gmail.com>
2025-09-15 21:06:28 +0530

    https://leetcode.com/problems/pascals-triangle/description/

--- DSA/Arrays/Medium/pascals_triangle.py
+++ DSA/Arrays/Medium/pascals_triangle.py
@@ -1,10 +1,25 @@
 from typing import List
 
-def generate_pascal_triangle(num_rows: int) -> List[List[int]]:
+def generate_pascal_triangle(n : int) -> List[List[int]]:
     """
     Generates the first num_rows of Pascal's triangle.
     """
-    pass
+    output = [[1]]
+    if n == 1:
+        return [[1]]
+
+    for i in range(1, n):
+        smallOutput = [1]
+
+        compute_list = output[-1]
+
+        for j in range(len(compute_list) - 1):
+            smallOutput.append(compute_list[j] + compute_list[j + 1])
+
+        smallOutput.append(1)
+        output.append(smallOutput)
+    return output
+    
 
 if __name__ == "__main__":
     num_rows = 5

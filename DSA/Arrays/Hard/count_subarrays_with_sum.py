25b8397ff6ba49d671f8103448a4f2ec46161860
Rajat Pandey <irajatnitjsr@gmail.com>
2025-09-16 22:05:48 +0530

    https://leetcode.com/problems/subarray-sum-equals-k/

--- DSA/Arrays/Hard/count_subarrays_with_sum.py
+++ DSA/Arrays/Hard/count_subarrays_with_sum.py
@@ -4,7 +4,18 @@ def count_subarrays_with_sum(nums: List[int], k: int) -> int:
     """
     Counts the number of subarrays with a given sum K.
     """
-    pass
+    mp = {0 : 1}
+    count = 0
+    current_sum = 0
+
+    for val in nums:
+        current_sum += val
+        if current_sum - k in mp:
+            count += mp[current_sum - k]
+
+        mp[current_sum] = mp.get(current_sum, 0) + 1
+
+    return count
 
 if __name__ == "__main__":
     nums = [1, 2, 3]

b2531de99340e82f9492f3ba7b81aabbecc619e4
Rajat Pandey <irajatnitjsr@gmail.com>
2025-09-18 20:26:13 +0530

    Longest SubArrayWithSumK

--- DSA/Arrays/Hard/longest_subarray_with_sum_k_positives.py
+++ DSA/Arrays/Hard/longest_subarray_with_sum_k_positives.py
@@ -1,11 +1,16 @@
 from typing import List
 
 def longest_subarray_with_sum_k_positives(nums: List[int], k: int) -> int:
-    """
-    Finds the length of the longest subarray with a given sum K.
-    (Assumes all numbers are positive)
-    """
-    pass
+    maxLength = 0
+    for i in range(len(nums)):
+        currentSum = 0
+        for j in range(i, len(nums)):
+            currentSum += nums[j]
+            if currentSum == k:
+                maxLength = max(maxLength, j - i + 1)
+    return maxLength
+
+
 
 if __name__ == "__main__":
     nums = [1, 2, 3, 1, 1, 1, 1, 3]

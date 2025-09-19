16ea9f3e3a71dca69c97786f26e540d60ac9876e
Rajat Pandey <irajatnitjsr@gmail.com>
2025-09-19 22:32:29 +0530

    https://leetcode.com/problems/merge-sorted-array/description/

--- DSA/Arrays/Hard/merge_sorted_arrays.py
+++ DSA/Arrays/Hard/merge_sorted_arrays.py
@@ -5,7 +5,23 @@ def merge(nums1: List[int], m: int, nums2: List[int], n: int) -> None:
     Merges nums2 into nums1 without using extra space.
     Do not return anything, modify nums1 in-place instead.
     """
-    pass
+
+    p1 = m - 1
+    p2 = n - 1
+    p3 = m + n - 1
+
+    while p2 >= 0:
+        if p1 >= 0 and nums1[p1] > nums2[p2]:
+            nums1[p3] = nums1[p1]
+            p1 -= 1
+        else:
+            nums1[p3] = nums2[p2]
+            p2 -= 1
+        p3 -= 1
+
+    
+
+    
 
 if __name__ == "__main__":
     nums1 = [1, 2, 3, 0, 0, 0]

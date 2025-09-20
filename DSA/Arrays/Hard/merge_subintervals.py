c1e35912872e55c640907d73bd9b0e5499e0b8ba
Rajat Pandey <irajatnitjsr@gmail.com>
2025-09-20 11:30:36 +0530

    https://leetcode.com/problems/merge-intervals/description/

--- DSA/Arrays/Hard/merge_subintervals.py
+++ DSA/Arrays/Hard/merge_subintervals.py
@@ -4,7 +4,19 @@ def merge(intervals: List[List[int]]) -> List[List[int]]:
     """
     Merges all overlapping intervals in a list.
     """
-    pass
+    intervals.sort()
+    output = []
+    output.append(intervals[0])
+
+    for i in range(1, len(intervals)):
+        last_merged = output[-1]
+        current_interval = intervals[i]
+
+        if current_interval[0] <= last_merged[1]:
+            last_merged[1] = max(last_merged[1], current_interval[1])
+        else:
+            output.append(current_interval)
+    return output
 
 if __name__ == "__main__":
     intervals = [[1, 3], [2, 6], [8, 10], [15, 18]]

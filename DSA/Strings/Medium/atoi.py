e15a00dd44e733c4251721d47f17ee7bcd10a828
Rajat Pandey <irajatnitjsr@gmail.com>
2025-09-21 19:21:20 +0530

    https://leetcode.com/problems/string-to-integer-atoi/description/

--- DSA/Strings/Medium/atoi.py
+++ DSA/Strings/Medium/atoi.py
@@ -1,9 +1,53 @@
 # implement_atoi.py
-
+import math
 class Solution:
+    # def remove_spaces(self, s : str) -> None:
+    #     i = 0
+    #     n = len(s)
+    #     s
+    #     while i < n and s[i] == ' ' or s[i] == '-' or s[i] == '+':
+    #           i += 1
+    #     j = n - 1
+
+    #     while j >= 0 and s[j] == ' ' or not s[j].isdigit():
+    #         j -= 1
+    #     return s[i : j + 1]
+
+
     def myAtoi(self, s: str) -> int:
-        # TODO: implement
-        return 0
+        n = len(s)
+        i = 0
+        sign = 1
+        output = 0
+
+        while i < n and s[i] == ' ':
+            i += 1
+        
+        if i == n:     ## testcase s = " ", output : 0
+            return 0
+        
+        if s[i] == '-':
+            sign = -1
+            i += 1
+        elif s[i] == '+':
+            i += 1
+
+        while i < n and s[i].isdigit():
+            digit = int(s[i])
+            output = output * 10 + digit
+            i += 1
+        
+        output = output * sign
+        max_int = int(math.pow(2, 31)) - 1
+        min_int = int(-math.pow(2, 31))
+        
+        if output > max_int:
+            return max_int
+        elif output < min_int:
+            return min_int
+        
+        return output
+        
 
 if __name__ == "__main__":
     sol = Solution()

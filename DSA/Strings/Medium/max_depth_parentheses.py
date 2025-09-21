e0d2cd34642b350236409c772e9bd23b69769b7f
Rajat Pandey <irajatnitjsr@gmail.com>
2025-09-21 14:16:10 +0530

    https://leetcode.com/problems/maximum-nesting-depth-of-the-parentheses/description/

--- DSA/Strings/Medium/max_depth_parentheses.py
+++ DSA/Strings/Medium/max_depth_parentheses.py
@@ -2,8 +2,15 @@
 
 class Solution:
     def maxDepth(self, s: str) -> int:
-        # TODO: implement
-        return 0
+        depth = 0
+        max_depth = 0
+        for char in s:
+            if char == '(':
+                depth += 1
+            elif char == ')':
+                depth -= 1
+            max_depth = max(max_depth, depth)
+        return max_depth
 
 if __name__ == "__main__":
     sol = Solution()

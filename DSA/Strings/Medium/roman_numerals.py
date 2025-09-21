d902827dc1650bcf92415e1d51cbf76fae54de44
Rajat Pandey <irajatnitjsr@gmail.com>
2025-09-21 18:07:20 +0530

    https://leetcode.com/problems/roman-to-integer/

--- DSA/Strings/Medium/roman_numerals.py
+++ DSA/Strings/Medium/roman_numerals.py
@@ -2,12 +2,29 @@
 
 class Solution:
     def romanToInt(self, s: str) -> int:
-        # TODO: implement
-        return 0
+        mapp = {
+            'I' : 1,
+            'V' : 5,
+            'X' : 10,
+            'L' : 50,
+            'C' : 100,
+            'D' : 500,
+            'M' : 1000
+        }
+        output = 0
+        n = len(s)
+        for i in range(n - 1):
+            current_char = mapp[s[i]]
+            next_char = mapp[s[i + 1]]
 
-    def intToRoman(self, num: int) -> str:
-        # TODO: implement
-        return ""
+            if current_char < next_char:
+                output -= current_char
+            else:
+                output += current_char
+        output += mapp[s[n - 1]]
+        return output
+        
+
 
 if __name__ == "__main__":
     sol = Solution()
@@ -16,6 +33,3 @@ if __name__ == "__main__":
     for r in roman_tests:
         print(f"romanToInt in : {r}")  # [web:95][web:89]
         print(f"romanToInt out: {sol.romanToInt(r)}")  # [web:95][web:89]
-    for n in int_tests:
-        print(f"intToRoman in : {n}")  # [web:86][web:95]
-        print(f"intToRoman out: {sol.intToRoman(n)}")  # [web:86][web:95]

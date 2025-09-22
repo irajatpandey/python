138bb6570c7942056ba58c93bafd5e0e5f19a9f0
Rajat Pandey <irajatnitjsr@gmail.com>
2025-09-22 19:40:37 +0530

    https://leetcode.com/problems/count-and-say/

--- DSA/Strings/Hard/count_and_say.py
+++ DSA/Strings/Hard/count_and_say.py
@@ -2,8 +2,23 @@
 
 class Solution:
     def countAndSay(self, n: int) -> str:
-        # TODO: implement
-        return ""
+        if n == 1:
+            return "1"
+        
+        smallOutput = self.countAndSay(n - 1)
+        i = 1
+        count = 1  # Count 1 se shuru hoga
+        ans = ""
+        
+        while i < len(smallOutput):
+            if smallOutput[i] == smallOutput[i-1]:
+                count += 1
+            else:
+                ans = ans + str(count) + smallOutput[i-1] # Pichle group ko add karo
+                count = 1  # Naye group ke liye count reset
+            i += 1
+        ans = ans + str(count) + smallOutput[i-1]
+        return ans
 
 if __name__ == "__main__":
     sol = Solution()

a83b7af401682bea58562b633fc6cd0c91524c08
Rajat Pandey <irajatnitjsr@gmail.com>
2025-09-23 19:48:39 +0530

    https://leetcode.com/problems/longest-palindromic-substring/

--- DSA/Strings/Hard/longest_pal_substring.py
+++ DSA/Strings/Hard/longest_pal_substring.py
@@ -2,8 +2,31 @@
 
 class Solution:
     def longestPalindrome(self, s: str) -> str:
-        # TODO: implement (e.g., expand around centers)
-        return ""
+        
+        def expand_from_center(left: int, right: int) -> str:
+            while left >= 0 and right < len(s) and s[left] == s[right]:
+                left -= 1
+                right += 1
+            # Return the palindrome found
+            return s[left + 1 : right]
+        
+        if len(s) <= 1:
+            return s
+            
+        longest = ""
+        
+        for i in range(len(s)):
+            # Odd length palindrome
+            odd_palindrome = expand_from_center(i, i)
+            if len(odd_palindrome) > len(longest):
+                longest = odd_palindrome
+            
+            # Even length palindrome
+            even_palindrome = expand_from_center(i, i + 1)
+            if len(even_palindrome) > len(longest):
+                longest = even_palindrome
+                
+        return longest
 
 if __name__ == "__main__":
     sol = Solution()

4c9d190b7b876e4e80da0c74e2465449344447cc
Rajat Pandey <irajatnitjsr@gmail.com>
2025-09-25 12:59:56 +0530

    https://www.geeksforgeeks.org/problems/number-of-occurrence2259/1

--- DSA/BinarySearch/Medium/count_occurrences.py
+++ DSA/BinarySearch/Medium/count_occurrences.py
@@ -1,6 +1,45 @@
+def lower_bound(arr: list[int], target: int):
+    n = len(arr)
+    start = 0
+    end = n - 1
+    ans = -1
+    while start <= end:
+        mid = start + (end - start) // 2
+        if arr[mid] == target:
+            ans = mid
+            end = mid - 1
+        elif arr[mid] > target:
+            end = mid - 1
+        else:
+            start = mid + 1
+    return ans
+
+def upper_bound(arr: list[int], target: int):
+    n = len(arr)
+    start = 0
+    end = n - 1
+    ans = -1
+    while start <= end:
+        mid = start + (end - start) // 2
+        if arr[mid] == target:
+            ans = mid
+            start = mid + 1
+        elif arr[mid] > target:
+            end = mid - 1
+        else:
+            start = mid + 1
+    return ans
+
 def count_occurrences(arr: list[int], target: int) -> int:
     # Your solution logic goes here
-    pass
+
+     upperBound =  upper_bound(arr, target) 
+     lowerBound =  lower_bound(arr, target) 
+
+     if lowerBound == -1:
+         return 0
+     return upperBound - lowerBound + 1
+   
 
 def run_tests_count():
     test_cases = [
@@ -16,3 +55,6 @@ def run_tests_count():
         print(f"Array: {arr}, Target: {target}")
         print(f"Expected: {expected}, Got: {result}")
         print("-" * 20)
+
+
+run_tests_count()

6ec2662d4a1d13a46770c107b2ad9334e6cf3b56
Rajat Pandey <irajatnitjsr@gmail.com>
2025-09-27 12:07:42 +0530

    https://www.geeksforgeeks.org/problems/find-nth-root-of-m5843/1

--- DSA/BinarySearch/Medium/nth_root.py
+++ DSA/BinarySearch/Medium/nth_root.py
@@ -1,5 +1,5 @@
 # nth_root.py - Find the Nth root of a number using binary search
-
+import math
 def find_nth_root(n: int, m: int) -> float:
     """
     Stub function to find the Nth root of number M using binary search.
@@ -7,7 +7,24 @@ def find_nth_root(n: int, m: int) -> float:
     """
     # Example: If n=3 (cube root) and m=27, the result should be 3.
     # Returns a float for precision, but often an integer if exact root is required.
-    return 0.0
+
+    if n == 1:
+        return 1.0
+    
+    start = 1
+    end = m
+    ans = -1
+    while start <= end:
+        mid = start + (end - start) // 2 
+        if pow(mid, n) == m:
+            return float(mid)
+
+        elif mid ** n > m:
+            end = mid - 1
+        else:
+            ans = mid
+            start = mid + 1
+    return ans
 
 if __name__ == "__main__":
     test_cases = [
